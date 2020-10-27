8bd1643ef732c0fc1a3bcb1ef4f5597234bfaa7d
rnkwilliams <rnkwilliams07@gmail.com>
2020-10-27 15:06:38 -0500

    completed exercises

--- weave/index.js
+++ weave/index.js
@@ -24,6 +24,18 @@
 
 const Queue = require('./queue');
 
-function weave(sourceOne, sourceTwo) {}
+function weave(sourceOne, sourceTwo) {
+  const merge = new Queue();
+
+  while (sourceOne.peek() || sourceTwo.peek()) {
+    if (sourceOne.peek()) {
+      merge.add(sourceOne.remove());
+    }
+    if (sourceTwo.peek()) {
+      merge.add(sourceTwo.remove());
+    }
+  }
+  return merge
+}
 
 module.exports = weave;
